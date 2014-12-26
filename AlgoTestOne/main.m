a5825425aa29b7dd02c8fcee07acd5905ddfd70b
kervs <kervins.valcourt@gmail.com>
2014-12-26 00:06:40 -0500

    Initial Commit

---
+++ AlgoTestOne/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AlgoTestOne
+//
+//  Created by Kervins Valcourt on 12/26/14.
+//  Copyright (c) 2014 EastoftheWestEnd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
